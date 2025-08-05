8b4119314ccf44a1e41d18e5d13cea027225c63a
Synthia Islam <synthia.mist@gmail.com>
2025-08-05 12:55:47 +0930

    [Week 2] Added Movie DB Context class and scaffolded Controller Class
    Signed-off-by: Synthia Islam <synthia.mist@gmail.com>

---
+++ Synthia_S375728/Week_2/MovieApplication/MovieApplication/Models/MovieDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieApplication.Models
+{
+    public class MovieDbContext: DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
