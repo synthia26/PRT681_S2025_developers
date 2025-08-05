2760d41ee0416d4f4511b1066bbb81a3f46f18be
Shaikat0079 <baruashaikat00790@gmail.com>
2025-08-05 19:06:59 +0930

    Function

---
+++ shaikat_S373812/Week_2/AreaOfTriangle/AreaOfTriangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfTriangle
+{
+    internal class Program
+    {
+        /*
+         * 
+         * Ask user for width and height, store them
+         * Create function to calculate the area
+         * Function should calculate the area using: (width * height) / 2
+         * Call in main and print out the area of  the triangle
+         * 
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+            int height = ReadInt("Enter height:");
+            //Console.WriteLine(width);
+            //Console.WriteLine(height);
+            int result = CalcArea(width, height);
+            Console.WriteLine($"The area of the triangle is: {result}");
+        }
+
+        static int ReadInt(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        static int CalcArea(int width, int height)
+        {
+            return (width * height) / 2;
+        }
+    }
+}
---
+++ shaikat_S373812/Week_2/ReturnType/ReturnType/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ReturnName();
+            PrintName();
+            Console.WriteLine(Add());
+        }
+        static string ReturnName()
+        {
+            return "John Doe";
+        }
+        static void PrintName()
+        {
+            //string name = ReturnName();
+            Console.WriteLine(ReturnName());
+        }
+        static int Add()
+        {
+            return 5 + 5;
+        }
+    }
+}
