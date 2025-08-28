90566c6081f4139de0fed1156800a2c880fd3dc7
retyez <karkeeritiz11@gmail.com>
2025-08-28 21:01:10 +0930

    creating context

---
+++ Ritiz_S372192/task2/BookStoreApp/DAL/EfDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.DAL
+{
+    public class EfDbContext : DbContext
+    {
+        public EfDbContext(DbContextOptions<EfDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Printer> Printers { get; set; }
+    }
+}
